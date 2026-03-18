4f72f938f82d9e1327d8288654bf99a81b7d4230
Okienak <cidmomo19@gmail.com>
2026-03-19 02:00:26 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,16 +5,16 @@ export default {
 
     // --- 1. API BUAT LINK (SIMPAN KE KV) --
     if (request.method === "POST" && path === "/api/create") {
-      // PERBAIKAN: Ambil juga data 'password' dari frontend
-      const { content, password } = await request.json();
+      // Sekarang HANYA menerima content (tanpa password)
+      const { content } = await request.json();
       
       if (!content) return new Response("Empty", { status: 400 });
 
       // Bikin ID 6 Karakter Acak
       const id = Math.random().toString(36).substring(2, 8);
 
-      // PERBAIKAN: Simpan 'content' DAN 'password' ke dalam KV Database
-      await env.PASTE_DB.put(id, JSON.stringify({ content, password }), { expirationTtl: 2592000 });
+      // Simpan content ke dalam KV Database (Tahan 30 Hari)
+      await env.PASTE_DB.put(id, JSON.stringify({ content }), { expirationTtl: 2592000 });
 
       return new Response(JSON.stringify({ id }), { headers: { "Content-Type": "application/json" } });
     }
@@ -26,7 +26,7 @@ export default {
 
       if (!data) return new Response("{}", { status: 404 });
 
-      // SULAP CACHE: Data disimpan di server Cloudflare (Edge) selama 30 Hari!
+      // Cache dari Cloudflare
       return new Response(data, {
         headers: {
           "Content-Type": "application/json",
@@ -36,7 +36,6 @@ export default {
     }
 
     // --- 3. ROUTING HALAMAN HTML ---
-    // Semua URL selain API bakal nampilin isi index.html (SPA Mode)
     let response = await env.ASSETS.fetch(request);
     if (response.status === 404) {
       const indexUrl = new URL(url);
@@ -45,4 +44,4 @@ export default {
     }
     return response;
   }
-};p
+};
